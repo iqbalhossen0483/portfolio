e61283c251769520662bd1c32205315acccd27d7
unknown <rayhan.senbag.bd@gmail.com>
2022-10-14 23:22:53 +0600

    header optimized

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,65 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 
 const Header = () => {
-  const [stiky, setStiky] = useState(false);
   const location = useLocation();
   const hash = location.hash;
 
-  const scroll = () => {
-    if (window.scrollY >= 80) {
-      setStiky(true);
-    } else {
-      setStiky(false);
-    }
-  };
-  const goTop = () => {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  };
-  window.addEventListener("scroll", scroll);
+  const menus = [
+    { txt: "Home", hash: "#top" },
+    { txt: "About", hash: "#about-container" },
+    { txt: "Service", hash: "#service" },
+    { txt: "Portfolio", hash: "#project-container" },
+    { txt: "Contact", hash: "#contact-container" },
+  ];
 
   return (
-    <div className={`${stiky ? "sticky shadow-md" : ""} header-wrapper`}>
+    <div className='header-wrapper'>
       <div className='col-span-2'>
         <div className='menu-container'>
           <img className='w-12' src='./logo.png' alt='' />
-          <a
-            className={hash === "#top" ? "menu-active" : ""}
-            onClick={goTop}
-            href='#top'
-          >
-            Home
-          </a>
-          <a
-            className={hash === "#about-container" ? "menu-active" : ""}
-            href='#about-container'
-          >
-            About
-          </a>
-          <a
-            className={hash === "#service" ? "menu-active" : ""}
-            href='#service'
-          >
-            Service
-          </a>
-          <a
-            className={hash === "#project-container" ? "menu-active" : ""}
-            href='#project-container'
-          >
-            Portfolio
-          </a>
-          <a
-            className={`${
-              hash === "#contact-container" ? "menu-active" : ""
-            } hidden md:block`}
-            href='#contact-container'
-          >
-            Contact
-          </a>
+          {menus.map((menu, i) => (
+            <a
+              className={`${
+                hash === menu.hash ? "menu-active" : ""
+              } hidden md:block`}
+              href={menu.hash}
+              key={i}
+            >
+              {menu.txt}
+            </a>
+          ))}
         </div>
       </div>
 
