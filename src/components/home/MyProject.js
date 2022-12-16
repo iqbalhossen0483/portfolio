d19eac5e45c3d3d32897d478362b2b50f411f81e
unknown <rayhan.senbag.bd@gmail.com>
2022-12-16 18:22:03 +0600

    updated

--- src/components/home/MyProject.js
+++ src/components/home/MyProject.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import data from "../../services/data";
 
 const MyProject = () => {
+  const [number, setNumber] = useState([0, 6]);
+  const go = useRef(null);
+
   return (
     <div id='project-container'>
       <header>
@@ -10,7 +13,7 @@ const MyProject = () => {
         <p className='tag'>Here I mentioned some of my projects</p>
       </header>
       <div className='project-wrapper'>
-        {data.projects.map((project, i) => (
+        {data.projects.slice(number[0], number[1]).map((project, i) => (
           <div className='project-item' key={i}>
             <img src={project.img} alt='' />
             <div>
@@ -20,6 +23,15 @@ const MyProject = () => {
                 {project.technology.join(", ")}
               </p>
               <div className='flex flex-wrap gap-x-4 gap-y-2 mt-3'>
+                <a
+                  className='demo-btn'
+                  href={project.demo}
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <span>Demo</span>
+                  <i className='fa fa-desktop' aria-hidden='true' />
+                </a>
                 <a href={project.codeF} target='_blank' rel='noreferrer'>
                   <span>Code F</span>
                   <i className='fa fa-code' aria-hidden='true' />
@@ -30,14 +42,28 @@ const MyProject = () => {
                     <i className='fa fa-code' aria-hidden='true' />
                   </a>
                 )}
-                <a href={project.demo} target='_blank' rel='noreferrer'>
-                  <span>Demo</span>
-                  <i className='fa fa-desktop' aria-hidden='true' />
-                </a>
               </div>
             </div>
           </div>
         ))}
+      </div>
+      <div className='flex justify-end mr-10 mt-5'>
+        <button
+          onClick={() => {
+            setNumber((prev) => {
+              if (prev[1] === data.projects.length) {
+                go.current.click();
+                return [0, 6];
+              } else return [0, data.projects.length];
+            });
+          }}
+          className='see-more-btn'
+        >
+          {number[1] === data.projects.length ? "Show less" : "Show More"}
+        </button>
+        <a hidden ref={go} href='#project-container'>
+          go
+        </a>
       </div>
     </div>
   );
